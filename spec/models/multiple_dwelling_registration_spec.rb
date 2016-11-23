e5baded203da6217d746b56766011150f3e12c45
Rennan Oliveira <renmcoliver@gmail.com>
2016-11-23 20:55:58 -0200

    #7 Adding multiple dwelling specs

--- spec/models/multiple_dwelling_registration_spec.rb
+++ spec/models/multiple_dwelling_registration_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe MultipleDwellingRegistration, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    [:registration_id,
+     :boro,
+     :boro_id,
+     :block,
+     :lot,
+     :bin,
+     :building_id,
+     :house_number,
+     :low_house_number,
+     :high_house_number,
+     :zip_code,
+     :address_street,
+     :address_code,
+     :community_board,
+     :registered_on,
+     :registration_ends_on].each do |attr|
+        it { is_expected.to validate_presence_of attr }
+      end
+  end
 end

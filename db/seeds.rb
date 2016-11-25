676a2dba479f6096b85288aa2fd2cd9b2b2e918f
Rennan Oliveira <renmcoliver@gmail.com>
2016-11-25 13:48:35 -0200

    #11 Adding Neighbourhood groups to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,22 @@ room_types = [
 existing_room_types = RoomType.pluck(:name)
 RoomType.create(room_types.select {|r| !r[:name].in?(existing_room_types) })
 
-# Populating Neighbourhoods
-
-neighbourhoods = [
+# Populating Neighbourhood Groups
+neighbourhood_groups = [
     {name: 'Brooklyn'},
+    {name: 'Manhattan'},
+    {name: 'Bronx'},
+    {name: 'Queens'},
+    {name: 'Staten Island'}
 ]
+existing_neighbourhood_groups = NeighbourhoodGroup.pluck(:name)
+NeighbourhoodGroup.create(
+    neighbourhood_groups.
+        select {|r| !r[:name].in?(existing_neighbourhood_groups) }
+)
+
+
+
 
 
 

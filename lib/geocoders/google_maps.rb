eef86363a3dbab625da549dfdcb10e8508fbd89d
Rennan Oliveira <renmcoliver@gmail.com>
2016-11-27 12:41:45 -0200

    #10 Documenting geocoder

--- lib/geocoders/google_maps.rb
+++ lib/geocoders/google_maps.rb
@@ -5,6 +5,15 @@ module Geocoders
     include HTTParty
     base_uri 'https://maps.googleapis.com/maps/api'
 
+    # returns a hashwithindifferentaccess with geocode for given address
+    # Usage:
+    #   geocoder.location_for('street_number street_name, neighbourhood, state, zipcode', 'API_KEY')
+    #   => {
+    #       "status" => "OK",
+    #       "location" => {
+    #       "lat" => -22.9170402,
+    #       "lng" => -43.26039859999999
+    #      }
     def location_for(address_string, key)
       formatted_response(
           self.class.get('/geocode/json', query: {address: address_string.gsub(' ', '+'), key: key}).
